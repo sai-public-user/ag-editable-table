f7c91e21f711257bbbca64a586ccfcdd6cd1628b
sai-public-user <saimanideep@hotmail.com>
2023-07-05 21:17:15 +0530

    ag-editable-table-react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CustomTable from './components/CustomTable';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomTable />
+    </div>
+  );
+}
+
+export default App;
